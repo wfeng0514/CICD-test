9a58a97bc4c6f919bbbf6ffd01871e67e8885fe2
wfeng <3022568959@qq.com>
2026-07-16 09:46:06 +0800

    chore: add Prettier + ESLint VSCode integration
    - Add .prettierrc config
    - Add .prettierignore
    - Add eslint-config-prettier to avoid conflicts
    - Add .vscode/settings.json (format-on-save + ESLint hints)
    - Add format/format:check scripts to package.json

--- src/index.js
+++ src/index.js
@@ -30,7 +30,8 @@ function fibonacci(n) {
     throw new Error('Fibonacci is not defined for negative numbers');
   }
   if (n <= 1) return n;
-  let a = 0, b = 1;
+  let a = 0,
+    b = 1;
   for (let i = 2; i <= n; i++) {
     [a, b] = [b, a + b];
   }
